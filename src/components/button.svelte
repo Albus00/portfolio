131dad8e4a9547d9d86fcb735171d9e59e97b096
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2024-06-11 15:17:58 +0200

    Started on hero

---
+++ src/components/button.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let size = 'md';
+</script>
+
+<button
+	class={`bg-slate-950 text-white font-bold text-2xl py-6 px-24 rounded-2xl ${size === 'sm' ? 'text-sm' : 'text-base'}`}
+>
+	<slot />
+</button>
---
+++ src/routes/hero.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Button from '$components/button.svelte';
+	import albinkjellberg from '$lib/assets/albinkjellberg.png';
+</script>
+
+<!-- <section class="grid grid-cols-2 h-full">
+	<div class="content-center pl-20">
+		<h1 class="h1-variant inline-block text-[#f2d83e] mr-3">Hi!</h1>
+		<h1 class="inline-block">My name is Albin. I design and build web-apps and games.</h1>
+	</div>
+	<div class="h-full pt-16 overflow-hidden">
+		<img src={albinkjellberg} alt="Albin Kjellberg" />
+	</div>
+</section> -->
+
+<section class="h-full pt-24">
+	<div
+		class="bg-no-repeat bg-right-top h-full pl-20"
+		style="background-image: url({albinkjellberg});"
+	>
+		<div class=" pt-[12%] w-3/4">
+			<h1 class="h1-variant mr-3">Hi!</h1>
+			<h1>My name is Albin. I design and build web apps and games.</h1>
+		</div>
+		<div class="flex flex-col w-fit gap-y-4 pl-16 pt-10">
+			<Button>Web</Button>
+			<Button>Games</Button>
+			<Button>Contact</Button>
+		</div>
+	</div>
+</section>
