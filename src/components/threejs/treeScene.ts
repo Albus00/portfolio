1cd1c8eeddfce5d104e77e61a96ee6db1c78d6f2
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2024-06-25 00:40:59 +0200

    feat: Add 3D tree model to scene with leaves texture

--- src/components/threejs/treeScene.ts
+++ src/components/threejs/treeScene.ts
@@ -8,20 +8,68 @@ export default function treeScene(elementId: string) {
     console.error(`Element with id ${elementId} not found`);
     return;
   }
+
+  const scene = new THREE.Scene();
+
+  const leavesTexture = new THREE.TextureLoader().load("/textures/tree-leaves.png");
+  const leavesMat = new THREE.MeshBasicMaterial({
+    color: 0x22bb44, side: THREE.DoubleSide,
+    alphaTest: 0.5, alphaMap: leavesTexture
+  });
+
   const loader = new GLTFLoader();
-  loader.load('/models/tree.gltf', function (gltf) {
+  loader.load('/models/tree.glb', function (gltf) {
+    gltf.scene.rotation.y = Math.PI / 6;
+    const content = gltf.scene.children;
+
+    let leavesMesh: THREE.Mesh = new THREE.Mesh();
+    content.forEach((element: THREE.Object3D) => {
+      if (element.isObject3D && element.name === 'leaves') {
+        leavesMesh = element as THREE.Mesh;
+      }
+    });
+
+    if (leavesMesh.name === 'leaves') {
+      leavesMesh.material = leavesMat;
+    }
+
+    gltf.scene.castShadow = true;
+    gltf.scene.receiveShadow = true;
+
     scene.add(gltf.scene);
   }, undefined, function (error) {
     console.error(error);
   });
 
-  const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, renderElement.offsetWidth / renderElement.offsetHeight, 0.1, 1000);
+  camera.position.set(3, 4, 6);
+  camera.lookAt(0, 3, 0);
 
 
-  camera.position.z = 5;
+  //Create a DirectionalLight and turn on shadows for the light
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(0, 1, 0); //default; light shining from top
+  light.castShadow = true; // default false
+  scene.add(light);
+
+  //Set up shadow properties for the light
+  light.shadow.mapSize.width = 512; // default
+  light.shadow.mapSize.height = 512; // default
+  light.shadow.camera.near = 0.5; // default
+  light.shadow.camera.far = 500; // default
+
+  const planeGeometry = new THREE.PlaneGeometry(20, 1, 32, 32);
+  const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 })
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.receiveShadow = true;
+  scene.add(plane);
+
+  const helper = new THREE.CameraHelper(light.shadow.camera);
+  scene.add(helper);
 
   const renderer = new THREE.WebGLRenderer();
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
 
   renderer.setSize(renderElement.offsetWidth, renderElement.offsetHeight);
   renderElement.appendChild(renderer.domElement);
