f49fb87a75288157842487d1e9fb9a99802ef450
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2024-07-03 19:15:41 +0200

    updated category cards

--- src/components/categoryCard.svelte
+++ src/components/categoryCard.svelte
@@ -1,30 +1,34 @@
 <script lang="ts">
 	import Scene from '$components/scene.svelte';
-	import { onMouseEnter, onMouseLeave } from '$lib/functions/card';
+	// import { onMouseEnter, onMouseLeave } from '$lib/functions/card';
 
 	export let title: string;
 	export let model: string;
 
 	const containerId = title.toLowerCase().replace(' ', '-');
+
+	export const hoverToggle = (containerId: string) => {
+		const title = document.getElementById(`${containerId}-title`);
+		if (!title) return;
+		title.classList.toggle('hidden');
+	};
 </script>
 
 <a
-	class={'bg-dark-base overflow-hidden rounded-[3rem] flex flex-col py-10 text-white box-border max-h-full transition-all cursor-pointer hover:bg-black'}
+	class={'bg-dark-base overflow-hidden rounded-[3rem] py-10 text-white box-border max-h-full transition-transform ' +
+		'hover:bg-black hover:scale-125 hover:translate-y-24'}
 	id={containerId}
-	on:mouseenter={() => onMouseEnter(model, containerId)}
-	on:mouseleave={() => onMouseLeave(model, containerId)}
+	on:mouseenter={() => hoverToggle(containerId)}
+	on:mouseleave={() => hoverToggle(containerId)}
 	href={`/${containerId}`}
 >
 	<div class="flex justify-center w-full transition-all">
-		<Scene elementId={`${model}-scene`} className={'w-1/2 transition-all ease-out'} />
+		<Scene elementId={`${model}-scene`} className={'w-full transition-all ease-out'} />
 	</div>
 	<div
 		class="flex flex-col justify-start transition-all animate__animated animate__faster animate__fadeIn"
 		id={`${containerId}-div`}
 	>
-		<h1 class="text-6xl text-center font-bold m-auto mb-5">{title}</h1>
-		<p class="px-horizontal">
-			<slot />
-		</p>
+		<h1 class="text-6xl text-center font-bold m-auto mb-5" id={`${containerId}-title`}>{title}</h1>
 	</div>
 </a>
--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<footer class="text-white bg-light-dark px-8 py-4">
+<footer class="relative text-white bg-light-dark px-8 py-4 z-50">
 	<a href="https://www.flaticon.com/free-icons/right" title="right icons"
 		>Icons created by Freepik - Flaticon</a
 	>
