b57fd5fecca1793e7985c5b1714aed109b964f6a
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2024-07-03 19:34:47 +0200

    category selector and cards adapted for mobile

--- src/components/categoryCard.svelte
+++ src/components/categoryCard.svelte
@@ -15,20 +15,20 @@
 </script>
 
 <a
-	class={'bg-dark-base overflow-hidden rounded-[3rem] py-10 text-white box-border max-h-full transition-transform ' +
+	class={'bg-dark-base overflow-hidden rounded-[3rem] py-10 text-white box-border max-h-full h-fit transition-transform ' +
 		'hover:bg-black hover:scale-125 hover:translate-y-24'}
 	id={containerId}
 	on:mouseenter={() => hoverToggle(containerId)}
 	on:mouseleave={() => hoverToggle(containerId)}
 	href={`/${containerId}`}
 >
-	<div class="flex justify-center w-full transition-all">
+	<div class="w-full transition-all">
 		<Scene elementId={`${model}-scene`} className={'w-full transition-all ease-out'} />
 	</div>
 	<div
-		class="flex flex-col justify-start transition-all animate__animated animate__faster animate__fadeIn"
+		class="flex flex-row justify-center transition-all animate__animated animate__faster animate__fadeIn"
 		id={`${containerId}-div`}
 	>
-		<h1 class="text-6xl text-center font-bold m-auto mb-5" id={`${containerId}-title`}>{title}</h1>
+		<h1 class="text-6xl text-center font-bold" id={`${containerId}-title`}>{title}</h1>
 	</div>
 </a>
--- src/layout/selection.svelte
+++ src/layout/selection.svelte
@@ -14,13 +14,13 @@
 
 <section
 	style="--header: {headerHeight}"
-	class={'bg-black relative grid grid-cols-3 gap-x-20 px-horizontal box-border pt-28 pb-32 ' +
-		'handheld:grid-rows-3 handheld:grid-cols-1 handheld:px-8 handheld:py-8 handheld:gap-y-8'}
+	class={'bg-black relative grid grid-cols-3 gap-x-20 px-horizontal box-border pt-28 pb-32 items-center ' +
+		'handheld:grid-rows-3 handheld:grid-cols-1 handheld:px-8 handheld:py-16 handheld:gap-y-16'}
 	id="selection"
 >
 	<CategoryCard title="WEB APPS" model="laptop" />
 	<CategoryCard title="GAMES" model="tree" />
-	<CategoryCard title="OTHER PROJECTS" model="other" />
+	<CategoryCard title="OTHER" model="other" />
 </section>
 
 <style>
